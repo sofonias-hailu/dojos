a556c6c1dde4b3d590e462aa9e221d35c1fe4d00
sofonias-hailu <59507926+sofonias-hailu@users.noreply.github.com>
2020-01-04 18:31:40 +0200

    Update EquiTest.cpp
    Adding an implementation of the function

--- equi/cpp/EquiTest.cpp
+++ equi/cpp/EquiTest.cpp
@@ -1,9 +1,33 @@
 #include <gtest/gtest.h>
+#include <iostream>
+#include <numeric>
 
 int equi(int inputArr[], int arrayLen)
 {
-    // Find equilibrium index of inputArr
-    return 0;
+    int equiPt = -1;
+    
+    if (arrayLen > 0 && std::accumulate(inputArr,inputArr+arrayLen,0) == 0)
+    {
+        equiPt = 0;
+    }      
+    else if (arrayLen > 0 && std::accumulate(inputArr,inputArr+arrayLen-1,0) == 0)
+    {
+        equiPt = arrayLen-1;
+    }
+    else
+    {
+        for (int numElem = 2; numElem < arrayLen-1; numElem++)
+        {
+            const int iniSum = 0;
+            if (std::accumulate(inputArr,inputArr+numElem-1,iniSum) == std::accumulate(inputArr+numElem,inputArr+arrayLen,iniSum))
+            {
+                equiPt = numElem-1;
+                break;
+            }
+        }
+    }  
+    
+    return equiPt;
 }
 
 TEST(EquiTest, EmptyArrayReturnsZero)
